358c0a27772b1509a8cdfc8982231e7293c4b0e0
Alessio Cimarelli <alessio@dataninja.it>
2018-06-23 15:58:12 +0200

    Complete all request chain from country to single model via makes and trims

--- js/Api.js
+++ js/Api.js
@@ -3,11 +3,46 @@ let apiUrl = "https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getMakes"
 let paramsString = ''
 let finalUrl = apiUrl + paramsString
 
-$.getJSON(finalUrl, datiArrivati)
+$.getJSON(finalUrl, gotMakes)
 
 console.log("CHIAMATA PARTITA")
 
-function datiArrivati(data) {
+// Callback eseguita dopo la richiesta dei dati di un singolo modello
+function gotModel(data, idSelection) {
+    let model = data[0]
+    $("#" + idSelection).parent().find(".model-container").text(model.model_name + " by " + model.make_display + " (" + model.make_country + ")")
+}
+
+// Callback eseguita dopo la richiesta dell'elenco dei modelli di un make
+function gotModels(data, make, idSelection) {
+
+    let makeModels = data.Models;
+
+    $("#" + idSelection).find(".models-menu").empty()
+    makeModels.forEach(function (model) {
+        let template = '<a class="dropdown-item" href="#">' + model.model_name + '</a>'
+        $("#" + idSelection).find(".models-menu").append(template);
+    });
+
+    $('#' + idSelection).find(".models-menu .dropdown-item").click(function (event) {
+        let model = event.target.innerText
+
+        // Evento di TRIMS in base alla selezione di makes
+        $.getJSON("https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getTrims&make=" + make + "&model=" + model, function (data) {
+            console.log("Trims", make, model, data)
+            // Evento di MODEL in base al primo Trim ottenuto
+            $.getJSON("https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getModel&model=" + data.Trims[0].model_id, function (data) {
+                console.log("Model", data)
+                gotModel(data, idSelection)
+            })
+        })
+        event.preventDefault(); //non far risalire la pagina
+    })
+
+}
+
+function gotMakes(data) {
+
     let allMakes = data.Makes,
         countries = allMakes
             .map(function (make) { return make.make_country; })
@@ -26,7 +61,8 @@ function datiArrivati(data) {
 
 
     // filtro il paese selezionato dell utente - console.log(event) x vedere cosa clicca e poi filtro
-    $("#primary-selection .countries-menu .dropdown-item").click(function (event) {        //#countries-menu e .dropecc è il selettore css
+    $(".countries-menu .dropdown-item").click(function (event) {        //#countries-menu e .dropecc è il selettore css
+        let idSelection = $(this).parents(".dropdown-group").attr("id")
         let country = event.target.innerText            // event.target.innertext è il percorso che avviene quando l utente clicca (target)
         let countryMakes = allMakes.filter(
             function (make) {
@@ -36,31 +72,24 @@ function datiArrivati(data) {
         console.log(countryMakes)
 
         // Evento di MAKES in base alla selezione  di countries
-        $("#primary-selection .makes-menu").empty() // svuotare il contenitore
+        $('#' + idSelection).find(".makes-menu").empty() // svuotare il contenitore
         countryMakes.forEach(function (make) {
-            $("#primary-selection .makes-menu").append('<a class="dropdown-item" href="#">' + make.make_display + '</a>');
+            $('#' + idSelection).find(".makes-menu").append('<a class="dropdown-item" href="#">' + make.make_display + '</a>');
         });
+
+        $('#' + idSelection).find(".makes-menu .dropdown-item").click(function (event) {        //#countries-menu e .dropecc è il selettore css
+            let make = event.target.innerText            // event.target.innertext è il percorso che avviene quando l utente clicca (target)
+
+            // Evento di MODELS in base alla selezione  di makes
+            $.getJSON("https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getModels&make=" + make, function (data) {
+                console.log("Models by", make, data)
+                gotModels(data, make, idSelection)
+            })
+            event.preventDefault(); //non far risalire la pagina
+        })
+
         event.preventDefault(); //non far risalire la pagina
     })
-
-    //SECONDAY SELECTION
-    $("#secondary-selection .countries-menu .dropdown-item").click(function (event) {        //#countries-menu e .dropecc è il selettore css
-    let country = event.target.innerText            // event.target.innertext è il percorso che avviene quando l utente clicca (target)
-    let countryMakes = allMakes.filter(
-        function (make) {
-            return make.make_country === country; // === è una comparazione 
-        }
-    )
-    console.log(countryMakes)
-
-    // Evento di MAKES in base alla selezione  di countries
-    $("#secondary-selection .makes-menu").empty() // svuotare il contenitore
-    countryMakes.forEach(function (make) {
-        $("#secondary-selection .makes-menu").append('<a class="dropdown-item" href="#">' + make.make_display + '</a>');
-    });
-    event.preventDefault(); //non far risalire la pagina
-    })
-
 
 
 
